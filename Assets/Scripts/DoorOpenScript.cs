67468c9197c13aac0fb3a1bf52f1c3618bb1e2fc
Ronojoy Mitra <ronojoymitra@gmail.com>
2019-09-27 17:41:08 -0400

    Added Door Opening functionality

---
+++ Assets/Scripts/DoorOpenScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpenScript : MonoBehaviour
+{
+    public Rigidbody thisRigidbody;
+    public Vector3 startingTorque;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisRigidbody.AddTorque(startingTorque);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
