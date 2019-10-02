ef8e879bee7188baa6e24588a5c99f25195f4018
Ronojoy Mitra <ronojoymitra@gmail.com>
2019-10-02 12:42:54 -0400

    Added Visitor Script. Started working on visitor leaving

--- Assets/Scripts/PlayerInteractionScript.cs
+++ Assets/Scripts/PlayerInteractionScript.cs
@@ -8,29 +8,34 @@ public class PlayerInteractionScript : MonoBehaviour
 
     public bool isCloseToDoor;
     public bool doorIsOpened;
+    public bool ShutDoor;
     // Update is called once per frame
     public float doorRotation;
     void Update()
     {
         Ray PlayerRay = new Ray(transform.position, transform.forward);
-        float maxRayDistance = 8f;
-        Debug.DrawRay(PlayerRay.origin,PlayerRay.direction,Color.green);
-        RaycastHit PlayerHit = new RaycastHit();
+        float maxRayDistance = 3f;
+        Debug.DrawRay(PlayerRay.origin,PlayerRay.direction * maxRayDistance,Color.green);
         doorRotation = door.transform.localEulerAngles.y;
-
+        RaycastHit PlayerHit = new RaycastHit();
         //if (Vector3.Distance(transform.position, door.transform.position) < 10f)
         //{
         //    isCloseToDoor = true;
         //}
-        if (PlayerHit.transform != null && PlayerHit.transform.tag == "Door")
-        {
-            Debug.Log("settingIsCloseToDoor to true");
-            isCloseToDoor = true;
-        }
         if (PlayerHit.transform == null)
         {
             Debug.Log("SettingIsclosetoDoor to false");
             isCloseToDoor = false;
+        }
+        if (Physics.Raycast(PlayerRay, out PlayerHit, maxRayDistance))
+        {
+            
+            if (PlayerHit.transform != null && PlayerHit.transform.tag == "Door")
+            {
+                Debug.Log("settingIsCloseToDoor to true");
+                isCloseToDoor = true;
+            }
+            
         }
         //if (Vector3.Distance(transform.position, door.transform.position) > 10f)
         //{
@@ -42,6 +47,7 @@ public class PlayerInteractionScript : MonoBehaviour
             doorIsOpened = true;
             Debug.Log("Opening Door");
             door.SendMessage("DoorOpened");
+            ShutDoor = false;
         }
         if (doorRotation > 118 && Input.GetKeyDown(KeyCode.F) && isCloseToDoor)
         {
@@ -49,6 +55,7 @@ public class PlayerInteractionScript : MonoBehaviour
                 Debug.Log("DoorisClosed");
                 door.SendMessage("DoorClosed");
                 doorIsOpened = false;
+                ShutDoor = true;
             }
         }
 
---
+++ Assets/Scripts/VisitorScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisitorScript : MonoBehaviour
+{
+    public GameObject OutsidetheDoor;
+    public GameObject Player;
+    public float VMoveSpeed = 4f;
+    public bool leave = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       transform.position = Vector3.MoveTowards(transform.position, OutsidetheDoor.transform.position, VMoveSpeed * Time.deltaTime);
+
+        if (transform.position == OutsidetheDoor.transform.position)
+        {
+            Debug.Log("PlayDoorbell Sound");
+            leave = true;
+        }
+        if(leave == true && Player.GetComponent<PlayerInteractionScript>().ShutDoor == true)
+        {
+            //todo fix logic 
+        }
+    }
+}
